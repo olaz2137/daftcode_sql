c13e6ecb4ea5711bd68bb4c976bd27f616d936b1
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-08 21:36:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ async def categories():
 @app.get("/customers")
 async def customers():
     app.db_connection.row_factory = sqlite3.Row
-    customers = app.db_connection.execute("SELECT CustomerID, ContactName, Address, PostalCode, City, Country FROM Customers").fetchall()
+    customers = app.db_connection.execute("SELECT CustomerID, ContactName, CONCAT (Address, PostalCode, City, Country) FROM Customers").fetchall()
     return {"customers" : [{"id": x['CustomerID'], "name": x['ContactName'], "full_adress": f"{x['Address']} {x['PostalCode']} {x['City']} {x['Country']}"} for x in customers]}
 
