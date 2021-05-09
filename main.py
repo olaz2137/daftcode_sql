27dfcfb3342efe981c609325a1419220be1e6406
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-09 13:38:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,11 +40,14 @@ async def products(id:int):
     if data is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return data
+
 #4.3
 @app.get("/employees")
-async def employees(*, limit:int, offset:int, order:str):
-    if order not in {"first_name","last_name","city"}:
+async def employees(*, limit:int = -1, offset:int = 0, order:str = Query(None)):
+    if order not in {"first_name","last_name","city",None}:
         raise HTTPException(status_code=400, detail="Bad Request")
+    if order is None:
+        order = 'id'
     app.db_connection.row_factory = sqlite3.Row
     data = app.db_connection.execute("SELECT EmployeeID AS id,LastName AS last_name , FirstName AS first_name , City as city FROM Employees ORDER BY :order LIMIT :limit OFFSET :offset",{'order':order,'limit':limit,'offset':offset}).fetchall()
     return {"employees":data}
