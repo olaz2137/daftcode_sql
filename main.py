d038ff8b087c7667533644e2133f95988a3964e7
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-16 22:27:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -118,6 +118,6 @@ from fastapi import FastAPI
 
 from .views import router as northwind_api_router
 
-app = FastAPI()
+#app = FastAPI()
 
 app.include_router(northwind_api_router, tags=["northwind"])
