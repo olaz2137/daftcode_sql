e27f5c14f598cd86bcffd18b8689ea8eaf066e9a
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-17 13:27:59 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ router = APIRouter()
 async def get_supplier(supplier_id: PositiveInt, db: Session = Depends(get_db)):
     db_supplier = crud.get_supplier(db, supplier_id)
     if db_supplier is None:
-        raise HTTPException(status_code=404, detail="Shipper not found")
+        raise HTTPException(status_code=404, detail="Supplier not found")
     return db_supplier
 
 
