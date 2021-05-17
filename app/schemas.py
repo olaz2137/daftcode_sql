c71f3df3529883415341558b8b4a279685c4ab03
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-17 13:08:31 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -3,18 +3,19 @@ from pydantic import BaseModel, PositiveInt, constr, AnyUrl
 
 class Supplier(BaseModel):
 
-    SupplierID = PositiveInt
-    CompanyName = constr(max_length=40)
-    ContactName = constr(max_length=30)
-    ContactTitle = constr(max_length=30)
-    Address = constr(max_length=60)
-    City = constr(max_length=15)
-    Region = constr(max_length=15)
-    PostalCode = constr(max_length=10)
-    Country = constr(max_length=15)
-    Phone = constr(max_length=24)
-    Fax = constr(max_length=24)
-    HomePage = str
+    SupplierID: PositiveInt
+    CompanyName: Optional[constr(max_length=40)]
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    Region: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    Fax: Optional[constr(max_length=24)]
+    HomePage: Optional[str]
+
     
     class Config:
         orm_mode = True
