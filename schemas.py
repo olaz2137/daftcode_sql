a3e28a7cc74410a719365a1e7a6b6546831e591e
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-17 08:31:52 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel, PositiveInt, constr
+from pydantic import BaseModel, PositiveInt, constr, AnyUrl
 
 
 class Supplier(BaseModel):
-    SupplierID: PositiveInt
-#    CompanyName: constr(max_length=40)
-#    Phone: constr(max_length=24)
 
+    SupplierID = PositiveInt
+    CompanyName = constr(max_length=40)
+    ContactName = constr(max_length=30)
+    ContactTitle = constr(max_length=30)
+    Address = constr(max_length=60)
+    City = constr(max_length=15)
+    Region = constr(max_length=15)
+    PostalCode = constr(max_length=10)
+    Country = constr(max_length=15)
+    Phone = constr(max_length=24)
+    Fax = constr(max_length=24))
+    HomePage = AnyUrl
+    
     class Config:
         orm_mode = True
