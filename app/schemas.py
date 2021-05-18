7240eec4f9d4969a4daadfc83e50eea36132150f
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-18 20:08:49 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -45,3 +45,16 @@ class SupplierProducts(BaseModel):
     
     class Config:
         orm_mode = True
+        
+class NewRecord(BaseModel):
+    CompanyName: str
+    ContactName: Optional[constr(max_length=30)]
+    ContactTitle: Optional[constr(max_length=30)]
+    Address: Optional[constr(max_length=60)]
+    City: Optional[constr(max_length=15)]
+    PostalCode: Optional[constr(max_length=10)]
+    Country: Optional[constr(max_length=15)]
+    Phone: Optional[constr(max_length=24)]
+    
+    class Confid:
+        orm_mode = True
