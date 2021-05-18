ed3846fc97831091c05854cdf774d5d50ce156a8
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-18 20:14:24 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -30,6 +30,6 @@ async def get_supplier_products(supplier_id: PositiveInt, db: Session = Depends(
     return db_supplier_products
 
 @router.post("/suppliers", response_model=schemas.Supplier, status_code=201)
-async def post_suppliers(db: Session = Depends(get_db), new_record: schemas.NewRecord):
+async def post_suppliers(new_record: schemas.NewRecord, db: Session = Depends(get_db)):
     db_new_record = crud.post_suppliers(db, new_record)
     return crud.get_suppliers(db)
