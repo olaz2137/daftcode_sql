39a5db3bf5c795520320394ee46ae53d4e64e123
Aleksandra Zięba <58306361+olaz2137@users.noreply.github.com>
2021-05-18 15:44:02 +0200

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -11,3 +11,7 @@ def get_supplier(db: Session, supplier_id: int):
     return (
         db.query(models.Supplier).filter(models.Supplier.SupplierID == supplier_id).first()
     )
+
+def get_supplier_products(db: Session, supplier_id:int):
+    return db.query(models.Product).filter(models.Product.SupplierID == supplier_id).order_by(models.Product.product_id.desc()).all()
+    
